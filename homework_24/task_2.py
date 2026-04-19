be25681d4e723bf5defbd357d888944b5486609f
Kateryna <rinamajj@gmail.com>
2026-04-19 18:34:39 +0300

    Add Homework 24 task 2

---
+++ homework_24/task_2.py
@@ -0,0 +1,15 @@
+def is_balanced(expr):
+    stack = []
+    pairs = {'(': ')', '[': ']', '{': '}'}
+
+    for char in expr:
+        if char in '([{':
+            stack.append(char)
+        elif char in ')]}':
+            if not stack or stack.pop() != pairs[char]:
+                return False
+
+    return not stack
+
+print(is_balanced('()[]{}'))
+print(is_balanced('([)]'))
