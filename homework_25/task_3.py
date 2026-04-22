3d4a956182d1fa1c970f22fc1025e87f58ca93e7
Kateryna <rinamajj@gmail.com>
2026-04-23 00:42:27 +0300

    Add Homework 25 task 3

---
+++ homework_25/task_3.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, item):
+        new_node = Node(item)
+
+        if self.front is None:
+            self.front = self.rear = new_node
+            return
+
+        self.front.next = new_node
+        self.rear = new_node
+
+    def dequeue(self):
+        if self.front is None:
+            raise ValueError("Queue is empty")
+
+        value = self.front.data
+        self.front = self.front.next
+
+        if self.rear is None:
+            self.rear = None
+
+        return value
+
+    def peek(self):
+        if self.front is None:
+            return None
+        return self.front.data
+
+    def is_empty(self):
+        return self.front is None
