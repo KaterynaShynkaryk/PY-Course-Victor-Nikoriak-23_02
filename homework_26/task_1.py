fcc92f44b596e71f23105c6c5ccc4aaf2be586e0
Kateryna <rinamajj@gmail.com>
2026-04-24 01:06:04 +0300

    Add Homework 26 task 1

---
+++ homework_26/task_1.py
@@ -0,0 +1,14 @@
+''' Реалізувати алгоритм бінарного пошуку за допомогою рекурсії. '''
+
+def binary_search(arr, target, left, right):
+    if left > right:
+        return -1
+
+    mid = (left + right) // 2
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search(arr, target, mid + 1, right)
+    else:
+        return binary_search(arr, target, left, mid - 1)
