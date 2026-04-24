eb6fd17fa0b185307e92fe1e3974c7640ecebc73
Kateryna <rinamajj@gmail.com>
2026-04-25 01:33:24 +0300

    Correct Homework 26 task 3

--- homework_26/task_3.py
+++ homework_26/task_3.py
@@ -6,25 +6,50 @@ class HashTable:
         self.table = [None] * self.size
 
     def _hash(self, key):
-        return len(key) % self.size
+        return abs(hash(key)) % self.size
 
     def put(self, key, value):
         index = self._hash(key)
-        self.table[index] = (key, value)
+
+        if self.table[index] is None:
+            self.table[index] = []
+
+        for i, (k, v) in enumerate(self.table[index]):
+            if k == key:
+                self.table[index][i] = (key, value)
+                return
+
+        self.table[index].append((key, value))
 
     def get(self, key):
         index = self._hash(key)
+
         if self.table[index] is None:
             return None
-        return self.table[index][1]
+
+        for k, v in self.table[index]:
+            if k == key:
+                return v
+
+        return None
 
     def __contains__(self, key):
         index = self._hash(key)
-        return self.table[index] is not None and self.table[index][0] == key
+
+        if self.table[index] is None:
+            return False
+
+        for k, _ in self.table[index]:
+            if k == key:
+                return True
+
+        return False
 
     def __len__(self):
         count = 0
-        for item in self.table:
-           if item is not None:
-               count += 1
+
+        for bucket in self.table:
+           if bucket is not None:
+               count += len(bucket)
+
         return count
