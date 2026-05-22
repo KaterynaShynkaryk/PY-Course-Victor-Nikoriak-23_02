887a466b24a8bb9e6c9aed62edbc319175d1f2eb
Kateryna <rinamajj@gmail.com>
2026-05-22 21:01:38 +0300

    version after completing the homework 40

--- homework_40/notes/models.py
+++ homework_40/notes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    reminder = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name = 'notes',
+    )
+
+    def __str__(self):
+        return self.title
